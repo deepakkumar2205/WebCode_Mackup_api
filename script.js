e64101ecc4b64788b30aaf62ec19629b05463109
deepakkumar <dk44861@gmail.com>
2022-11-22 12:47:01 +0530

    "final changes"

--- script.js
+++ script.js
@@ -1,30 +1,103 @@
 //Target an element
 let ul=document.querySelector(".ul");
-let btn=document.querySelector("btn");
+let dropDown=document.getElementById("dropDown");
+
+//important brands list in array
+let brands=['Select brand here','almay','annabelle','benefit','cargo cosmetics','clinique','covergirl','e.l.f.',"l'oreal",'maybelline','nyx','smashbox','revlon'];
+
+//using forEach method to createelment and append in the dropdown list.
+brands.forEach((brand,index)=>{
+    let option=document.createElement("option");
+    option.setAttribute("value",index);
+    option.innerText=brand;
+    dropDown.append(option)
+});
+
+//initial brand name
+let brandName="maybelline";
+//dynamic url
+let url=(`https://makeup-api.herokuapp.com/api/v1/products.json?brand=${brandName}`);
+//starting page.
+let startpage=0;
+//eventlistener to listen the drop down selection
+dropDown.addEventListener("click",async()=>{
+    let index=dropDown.selectedIndex;
+     if(index>0){
+        brandName="";
+        brandName+=brands[index];
+        url=(`https://makeup-api.herokuapp.com/api/v1/products.json?brand=${brandName}`)
+        console.log(url);
+     }
+     try {
+        let res = await fetch(url);
+        res=await res.json();
+        let filteredArray=res.slice(startpage,startpage+9)
+            ul.innerHTML=ThreeCard(filteredArray);
+        } catch (error) {
+            console.log(error);        
+    }
+
+})
+
 //create function to fetch data from api
 let getUsers = async ()=>{
-    let url=(`https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline`);
   try {
       let res = await fetch(url);
       res=await res.json();
-      console.log(ThreeCard(res));
-      ul.innerHTML=ThreeCard(res);
-      
-    
+      return res
   } catch (error) {
       console.log(error);
   }
 }
-getUsers()
 
-//function to append the template to html page
-let inserting=(data)=>{
-    data.forEach(element => {
-        //  showProduct(element)   ;
-         
+//this will load the data when page get load.
+addEventListener("DOMContentLoaded",async ()=>{
+    try {
+        let res = await fetch(url);
+        res=await res.json();
+        let filteredArray=res.slice(startpage,startpage+9)
+            ul.innerHTML=ThreeCard(filteredArray);
+            // inserting(res);       
+        } catch (error) {
+            console.log(error);        
+    }
 })
-}
 
+//clicking next putton
+let nextBtn=document.querySelector(".nextBtn");
+nextBtn.addEventListener("click",()=>{
+    
+    startpage+=9;
+    getUsers().then(function(value){
+        console.log(startpage);
+        if(startpage<value.length){
+            let array=value.slice(startpage,startpage+9);
+            ul.innerHTML=ThreeCard(array);
+        }else{
+            startpage+=-9;
+            alert("you are already in last page")
+        }
+    }) 
+})
+
+//clicking prev putton
+let prevBtn=document.querySelector(".prevBtn");
+prevBtn.addEventListener("click",()=>{
+    
+    startpage-=9;
+    getUsers().then(function(value){
+        console.log(startpage);
+        if(startpage>0){
+            let array=value.slice(startpage,startpage+9);
+            ul.innerHTML=ThreeCard(array);
+        }else{
+            startpage+=+9;
+            alert("you are already in first page")
+        }
+    }) 
+})
+
+// this template funciton will give syntax of html with value
 let template=(product)=>{
     let temp=`
     <!-- CARD -->
@@ -36,20 +109,20 @@ let template=(product)=>{
                     <div class=align>
                         <p class="card-text"><b>Brand:</b>${product.brand}</p>
                         <p class="card-text"><b>Price :</b>$ ${(+product.price)+(-0.03)}</p>
-                        <p class="card-text"><b>Population :</b> ${(product.description)}</p>
+                        <p class="card-text"><b>Description :</b> ${(product.description)}</p>
                         <a class="card-text" href="${product.product_link}"><b>Click here to buy </b></a>
                     </div>
                 </div>
             </div>
     </div>
                     `;        
-            // <button class="btn btn-primary" onclick="weatherBtn([${country.latlng[0]},${country.latlng[1]}],'${country.name.common}')">weather</button>                       
+                     
         return temp
 }
 
-//let 3data
+//let 3card in a row
 let ThreeCard=(data)=>{
-    console.log(data.length);
+
     let mainAdd="";
     let tempAdd="";
     for(let i=0;i<data.length;i=i+3){
@@ -62,10 +135,5 @@ let ThreeCard=(data)=>{
     }
     return mainAdd
 }
-console.log("deepakkumar");
 
-
-
-
-
-///////////////////////////////////////////
+/////////////////end///////////////////////////////////
